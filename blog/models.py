36f0f9d9ad39f1ed6fbe9884a025f9540398834e
Zhelizko Daniel <yokzz.programming@gmail.com>
2024-03-17 14:22:08 +0200

    added templates and models

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,33 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
+class Author(models.Model):
+    name = models.CharField(max_length=63)
+    bio = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
     published_date = models.DateTimeField()
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING, related_name='post',
+                               default=None)
+    
+    def published_recently(self):
+        return timezone.now() - timedelta(days=7) < self.published_date
     
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    author_name = models.CharField(max_length=63)
+    text = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.DO_NOTHING, related_name='comments',
+                             default=None)
+    
+    def __str__(self):
+        return self.text
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 import blog.views as blog_views
 
 urlpatterns = [
-    path("", blog_views.post_list, name="post_list")
+    path("", blog_views.post_list, name="post_list"),
+    path("<int:post_id>/", blog_views.get_post_by_id, name="post_detail"),
+    path("author/<int:author_id>/", blog_views.get_post_by_author, name="author_posts")
 ]
